a3f542f6276e182ec6bc108ddb89ab0ccef4d69e
Alexander Mishenin <alexmishdev@gmail.com>
2021-07-22 19:12:55 +0300

    add DataBaseFile

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,18 @@
+//import DataBaseFile from "./database";
 const router = require("express").Router();
 // import function(controller)
 
+// test 
+const { DataBaseFile } = require("./database");
+//import dbt from "./database";
+
 router.get("/", (req, res) => {
     res.send("ger request !!!");
+    const DBFile = new DataBaseFile();
+    DBFile.read();
+    DBFile.write("hello word");
+    DBFile.write("--hello word--");
+    DBFile.readAll();
 });
 
 module.exports = router;
