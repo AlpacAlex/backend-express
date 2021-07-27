85445f2c41885bfc63879dc3b55e4ecb3b4bef7a
Alexander Mishenin <alexmishdev@gmail.com>
2021-07-27 10:58:44 +0300

    add order, filter, pagination

--- api/task/tasks.get.js
+++ api/task/tasks.get.js
@@ -1,51 +1,31 @@
 const router = require("express").Router();
 const assist = require("../../assistant/assist");
 const  { query, validationResult }  = require("express-validator");
+const LIMIT = 5;
 
 const isValid = () => {
     return [
         query("page").exists().isInt({ min: 1 }).toInt().withMessage("no page specified"),
-        query("orderBy").optional({ checkFalsy: true, nullable: true }).isString().withMessage("error me"),//custom( (value, {req}) => {console.log(value);}),
-        // query("filterBy").optional({ checkFalsy: true, nullable: true }).isString().custom( (value, {req}) => {
-        //     console.log(value);
-        // }),
-        //query("orderBy").exists().withMessage("not query order").isString().withMessage("not filter by"),
-        
     ]
 } 
-
+ 
 const getTasks = async (req, res, next) => {
     const er = validationResult(req);
     if (er.isEmpty()) {
         let todos = await assist.read();
         const {page, orderBy, filterBy} = req.query;
-
-        // if (typeof filterBy === "undefined") {
-        //     filterBy = "done";
-        // }
-
-        //let currentData = [];
-        todos = await assist.orderBy(orderBy, todos);
-        todos = await assist.filterBy(filterBy, todos);
-
-        // if(orderBy === "asc") {
-        //     todos = await assist.orderBy(true, todos);
-        // } else if (orderBy === "desc") {
-        //     todos = await assist.orderBy(false, todos);
-        // } else {
-        //     console.log("unknown order");
-        // } 
-        // if (filterBy === "done") {
-        //     todos = await assist.filterBy(true, todos);
-        // } else if (filterBy === "undone") {
-        //     todos = await assist.filterBy(false, todos);
-        // } else {
-        //     console.log("unknown filter");
-        // }
-        todos = assist.returnTasksOnPage(page, todos);
-
-         
-
+        if (typeof orderBy === "undefined") {
+            orderBy = "asc";
+        }
+        todos.sort( (a, b) =>  { return ( (orderBy === "asc") ? 
+            ( (Date.parse(a.createdAt) - Date.parse(b.createdAt)) ? 1 : -1) : 
+            ( (Date.parse(b.createdAt) - Date.parse(a.createdAt)) ? -1 : 1) )
+        });
+        todos = (typeof filterBy === "undefined") ? todos : todos.filter( (todo) => (todo.done === ((filterBy === "done") ? true : false)));
+        todos = todos.slice(
+                (page - 1) * LIMIT,
+                (page - 1) * LIMIT + LIMIT
+            );
         res.status(200).json(todos);
     } else {
         const firstError = er.errors[0].msg;
@@ -53,9 +33,7 @@ const getTasks = async (req, res, next) => {
     }
     
 }
-
+ 
 router.get("/", isValid(), getTasks);
-
+ 
 module.exports = router;
-
-
--- assistant/assist.js
+++ assistant/assist.js
@@ -20,47 +20,4 @@ async function remove(uuid) {
     return await DBFile.delete(uuid);
 }
 
-// async function filterBy(flag, todos) {
-//     //const filterArr = [...todos];
-//     const alredyFilter = todos.filter( (todo) => todo.done === ((flag === "done") ? true : false));
-//     return alredyFilter;
-// }
-
-// async function orderBy(flag, todos) {
-//     //const orderArr = [...todos];
-//     try {
-//         const alredyOrder = [...todos];
-//         //const d1 = Date.parse(a.createdAt) - Date.parse(b.createdAt);
-//         //const d2 = Date.parse(b.createdAt) - Date.parse(a.createdAt);
-//         alredyOrder.sort( (a, b) => {(
-//                 ((flag === "asc") ? 
-//                 (Date.parse(a.createdAt) - Date.parse(b.createdAt)) : 
-//                 (Date.parse(b.createdAt) - Date.parse(a.createdAt)))
-//             )}
-//         );
-//         // alredyOrder.sort( (flag === "asc") ?
-//         //     (a, b) => {Date.parse(a.createdAt) - Date.parse(b.createdAt) ? 1 : -1} :
-//         //     (a, b) => {Date.parse(b.createdAt) - Date.parse(a.createdAt) ? 1 : -1}
-//         //     );
-//         // alredyOrder.sort( (a, b) => {(flag ? 
-//         // Date.parse(a.createdAt) - Date.parse(b.createdAt) : 
-//         // Date.parse(b.createdAt) - Date.parse(a.createdAt))});
-        
-//         return alredyOrder;
-//     } catch (e) {
-//         console.log(e);
-//     }
-    
-// }
-
-// async function returnTasksOnPage(currentPage, todos) {
-//     const LIMIT = 5;
-//     const pageTodos = [...todos]
-//     .slice(
-//         (currentPage - 1) * LIMIT,
-//         (currentPage - 1) * LIMIT + LIMIT
-//       );
-//     return pageTodos;
-// }
-
 module.exports = {read, write, update, remove };
