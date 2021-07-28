b95f25d5ed8dad235504b4a2ef4ff43abaecc5eb
Alexander Mishenin <alexmishdev@gmail.com>
2021-07-28 15:08:28 +0300

    add read database

--- api/task/tasks.get.js
+++ api/task/tasks.get.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const assist = require("../../assistant/assist");
+//const assist = require("../../assistant/assist");
+const {Todos}  = require("../../models");
 const  { query, validationResult }  = require("express-validator");
 const { BaseError } = require("../../assistant/ApiError");
 const LIMIT = 5;
@@ -7,6 +8,7 @@ const LIMIT = 5;
 const isValid = () => {
     return [
         query("page").exists().withMessage("no exist page in get reqest").isInt({ min: 1 }).toInt().withMessage("no page specified"),
+        query("orderBy").default("asc"),
     ]
 } 
  
@@ -15,20 +17,14 @@ const getTasks = async (req, res, next) => {
         const er = validationResult(req);
         if (er.isEmpty()) {
             try {
-                let todos = await assist.read();
                 const {page, orderBy, filterBy} = req.query;
-                if (typeof orderBy === "undefined") {
-                    orderBy = "asc";
-                }
-                todos.sort( (a, b) =>  { 
-                    return ( (orderBy === "asc") ? (Date.parse(a.createdAt) - Date.parse(b.createdAt)) : (Date.parse(b.createdAt) - Date.parse(a.createdAt)))  
-                });
-                todos = (typeof filterBy === "undefined") ? todos : todos.filter( (todo) => (todo.done === ((filterBy === "done") ? true : false)));
-                todos = todos.slice(
-                        (page - 1) * LIMIT,
-                        (page - 1) * LIMIT + LIMIT
-                    );
-                res.status(200).json(todos);
+                const chosenTodos = await Todos.findAndCountAll({
+                    limit: LIMIT,
+                    offset: (page - 1) * LIMIT,
+                    order: [["createdAt", `${orderBy}`]],
+                    //where: false
+                })
+                res.status(200).json(chosenTodos);
             } catch (e) {
                 console.log(e);
                 next(BaseError.Error400("myerror"));
