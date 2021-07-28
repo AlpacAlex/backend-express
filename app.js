d13589fade6be615cd8d39c592e035cee11e28a4
Alexander Mishenin <alexmishdev@gmail.com>
2021-07-28 18:32:44 +0300

    add port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {handleError} = require("./assistant/ApiError");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors);
 app.use(express.json());
