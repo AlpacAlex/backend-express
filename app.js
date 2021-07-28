45e9e527c465c5d15b5ab1425dfec8661e259035
Alexander Mishenin <alexmishdev@gmail.com>
2021-07-28 18:21:41 +0300

    add DB

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { initMainRouter } = require("./config/root-routes");
 const {handleError} = require("./assistant/ApiError");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors);
 app.use(express.json());
 
 initMainRouter(app);
