6481794acc8d9e819f46ad8e28b62346770b12c9
Alexander Mishenin <alexmishdev@gmail.com>
2021-07-29 10:47:48 +0300

    add lib cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { initMainRouter } = require("./config/root-routes");
 const {handleError} = require("./assistant/ApiError");
+const cors =require("cors");
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors);
 app.use(express.json());
 
 initMainRouter(app);
