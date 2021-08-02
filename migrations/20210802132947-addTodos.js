24325697d1a60c9330200406d59c36fe9c0dbbae
Alexander Mishenin <alexmishdev@gmail.com>
2021-08-02 16:38:38 +0300

    add new column and migrated DB

---
+++ migrations/20210802132947-addTodos.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     return Promise.all([
+      queryInterface.addColumn(
+        'Todos', // table name
+        'userId', // new field name
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+      ),
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     return Promise.all([
+      queryInterface.removeColumn('Todos', 'userId'),
+    ]);
+
+  }
+};
